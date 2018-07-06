f890545abc541ddc9c4f5fd434c9003cee6927c3
Naomi Mitsumori <naomi.mitsumori@narvar.com>
2018-07-06 10:41:38 -0700

    This is my second commit, just a simple comment.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // This is a test comment. I'm practicing with Git!
         System.out.println("New additions!");
     }
 }
